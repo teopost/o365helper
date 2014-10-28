2a22b3d5f788e53d548ca1b092bafc219e40e369
Stefano Teodorani <teopost@gmail.com>
2014-10-28 20:53:42 +0100

    Initial commit

---
+++ O365Helper/O365Helper.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Security;
+using System.Management.Automation.Remoting;
+using System.Management.Automation.Runspaces;
+using System.Management.Automation;
+using System.Collections.ObjectModel;
+
+namespace O365Wrapper
+{
+    class O365Helper
+    {
+        private string _AdminUser = "";
+        private string _AdminPassword = "";
+
+        // UserData
+        public string userPrincipalName = "";
+        public string displayName = "";
+        public string firstName = "";
+        public string lastName = "";
+        public string licenseAssignment = "";
+        public string usageLocation = "";
+        public string Password = "";
+
+        public string LastMessage = "";
+        public string LastError = "";
+
+        public O365Helper(string AdminUser, string AdminPassword)
+        {
+
+            this._AdminUser = AdminUser;
+            this._AdminPassword = AdminPassword;
+        }
+
+        public bool CreateUser()
+        {
+            LastError = "";
+            LastMessage = "";
+
+            string scriptText = "import-module MSOnline\r\n" +
+            "$Username = '" + _AdminUser + "'\r\n" +
+            "$Password = ConvertTo-SecureString -AsPlainText '" + _AdminPassword + "' -Force\r\n" +
+            "$Livecred = New-Object System.Management.Automation.PSCredential $Username, $Password\r\n" +
+            "Connect-MsolService -Credential $Livecred\r\n" +
+              "New-MsolUser -UserPrincipalName '" + this.userPrincipalName + "' -DisplayName '" + this.displayName + "' -FirstName '" + this.firstName + "' -LastName '" + this.lastName + "' -UsageLocation '" + this.usageLocation + "' -Password '" + this.Password + "'\r\n";
+
+            //  "New-MsolUser -UserPrincipalName '" + userData.userPrincipalName + "' -DisplayName '" + userData.displayName + "' -FirstName '" + userData.firstName + "' -LastName '" + userData.lastName + "' -LicenseAssignment '" + userData.licenseAssignment + "' -UsageLocation '" + userData.usageLocation + "' -Password '" + userData.Password + "'\r\n";
+
+            PowerShell psExec = PowerShell.Create();
+            psExec.AddScript(scriptText);
+            //psExec.AddCommand("out-string");
+            Collection<PSObject> results;
+            Collection<ErrorRecord> errors;
+            results = psExec.Invoke();
+
+            StringBuilder tmpResult = new StringBuilder();
+            if (results != null && results.Count != 0)
+            {
+                foreach (PSObject obj in results)
+                {
+                    //stringBuilder.AppendLine(obj.ToString());
+                    tmpResult.AppendLine(obj.Members["DisplayName"].Value.ToString());
+                }
+                LastMessage = tmpResult.ToString();
+                return true;
+            }
+            else
+            {
+                errors = psExec.Streams.Error.ReadAll();
+                StringBuilder tmpErrorMsg = new StringBuilder();
+                if (errors != null)
+                {
+                    foreach (ErrorRecord obj1 in errors)
+                    {
+                        //stringBuilder.AppendLine(obj.ToString());
+                        tmpErrorMsg.AppendLine(obj1.Exception.Message.ToString());
+                    }
+                }
+                this.LastError = tmpErrorMsg.ToString();
+                return false;
+            }
+
+        }
+        public bool UpdateUser()
+        {
+
+            LastError = "";
+            LastMessage = "";
+
+            string scriptText = "import-module MSOnline\r\n" +
+            "$Username = '" + _AdminUser + "'\r\n" +
+            "$Password = ConvertTo-SecureString -AsPlainText '" + _AdminPassword + "' -Force\r\n" +
+            "$Livecred = New-Object System.Management.Automation.PSCredential $Username, $Password\r\n" +
+            "Connect-MsolService -Credential $Livecred\r\n" +
+              "Set-MsolUser -UserPrincipalName '" + this.userPrincipalName + "' -DisplayName '" + this.displayName + "' -FirstName '" + this.firstName + "' -LastName '" + this.lastName + "' -UsageLocation '" + this.usageLocation + "' -Password '" + this.Password + "'\r\n";
+
+            //  "New-MsolUser -UserPrincipalName '" + userData.userPrincipalName + "' -DisplayName '" + userData.displayName + "' -FirstName '" + userData.firstName + "' -LastName '" + userData.lastName + "' -LicenseAssignment '" + userData.licenseAssignment + "' -UsageLocation '" + userData.usageLocation + "' -Password '" + userData.Password + "'\r\n";
+
+            PowerShell psExec = PowerShell.Create();
+            psExec.AddScript(scriptText);
+            //psExec.AddCommand("out-string");
+            Collection<PSObject> results;
+            Collection<ErrorRecord> errors;
+            results = psExec.Invoke();
+
+            StringBuilder tmpResult = new StringBuilder();
+            if (results != null && results.Count != 0)
+            {
+                foreach (PSObject obj in results)
+                {
+                    //stringBuilder.AppendLine(obj.ToString());
+                    tmpResult.AppendLine(obj.Members["DisplayName"].Value.ToString());
+                }
+                LastMessage = tmpResult.ToString();
+                return true;
+            }
+            else
+            {
+                errors = psExec.Streams.Error.ReadAll();
+                StringBuilder tmpErrorMsg = new StringBuilder();
+                if (errors != null)
+                {
+                    foreach (ErrorRecord obj1 in errors)
+                    {
+                        //stringBuilder.AppendLine(obj.ToString());
+                        tmpErrorMsg.AppendLine(obj1.Exception.Message.ToString());
+                    }
+                }
+                this.LastError = tmpErrorMsg.ToString();
+                return false;
+            }
+
+        }
+
+        public bool DeleteUser(string UserPrincipalName)
+        {
+            LastError = "";
+            LastMessage = "";
+
+            string scriptText = "import-module MSOnline\r\n" +
+            "$Username = '" + _AdminUser + "'\r\n" +
+            "$Password = ConvertTo-SecureString -AsPlainText '" + _AdminPassword + "' -Force\r\n" +
+            "$Livecred = New-Object System.Management.Automation.PSCredential $Username, $Password\r\n" +
+            "Connect-MsolService -Credential $Livecred\r\n" +
+              "Remove-MsolUser -UserPrincipalName '" + this.userPrincipalName + "' -force \r\n";
+
+       
+            PowerShell psExec = PowerShell.Create();
+            psExec.AddScript(scriptText);
+            //psExec.AddCommand("out-string");
+            Collection<PSObject> results;
+            Collection<ErrorRecord> errors;
+            results = psExec.Invoke();
+
+            StringBuilder tmpResult = new StringBuilder();
+            if (results != null && results.Count != 0)
+            {
+                foreach (PSObject obj in results)
+                {
+                    //stringBuilder.AppendLine(obj.ToString());
+                    tmpResult.AppendLine(obj.Members["DisplayName"].Value.ToString());
+                }
+                LastMessage = tmpResult.ToString();
+                return true;
+            }
+            else
+            {
+                errors = psExec.Streams.Error.ReadAll();
+                StringBuilder tmpErrorMsg = new StringBuilder();
+                if (errors != null)
+                {
+                    foreach (ErrorRecord obj1 in errors)
+                    {
+                        //stringBuilder.AppendLine(obj.ToString());
+                        tmpErrorMsg.AppendLine(obj1.Exception.Message.ToString());
+                    }
+                    
+                }
+                this.LastError = tmpErrorMsg.ToString();
+                return false;
+            }
+
+        }
+    }
+}
---
+++ O365Helper/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Security;
+using System.Management.Automation.Remoting;
+using System.Management.Automation.Runspaces;
+using System.Management.Automation;
+using System.Collections.ObjectModel;
+
+/*
+ http://o365info.com/using-remote-powershell-to-manage_212/
+ http://techathon.mytechlabs.com/create-user-on-microsoft-outlookoffice365-using-c-with-powershell-api/
+ http://techathon.mytechlabs.com/microsoft-outlookoffice365-issue-or-error-with-64-bit-with-powershell-api/
+ */
+
+namespace O365Wrapper
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                string AdminUser = "";
+                string AdminPassword = "";
+
+                var o = new O365Helper(AdminUser, AdminPassword);
+
+                // Insert New user
+                // ----------------
+                o.userPrincipalName = "User09@didanet.onmicrosoft.com";
+                o.displayName = "User from c#";
+                o.firstName = "Nome";
+                o.lastName = "Cognome 2";
+                //  o.licenseAssignment = "";
+                o.Password = "pippo123!22";
+                o.usageLocation = "IT";
+
+                Console.WriteLine("Creating user...");
+                bool RetValCreate = o.CreateUser();
+
+                if (RetValCreate)
+                    Console.WriteLine("User is successfully created: " + o.LastMessage);
+                else
+                    Console.WriteLine(o.LastError);
+
+                Console.WriteLine("Press any key to continue");
+                Console.ReadLine();
+
+
+                // Delete user
+                // -----------
+                Console.WriteLine("Deleting user...");
+                bool RetValDelete = o.DeleteUser("User09@didanet.onmicrosoft.com");
+
+                if (RetValDelete)
+                    Console.WriteLine("User deleted: " + o.LastMessage);
+                else
+                    Console.WriteLine(o.LastError);
+                
+                
+                Console.WriteLine("Press any key to continue");
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+
+        }
+     
+
+    }
+
+
+
+}
